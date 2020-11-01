cbd7e0f5182e38b6fd4449880a43d2fc47835ba6
kandy <shpiglify@gmail.com>
2020-11-01 14:56:47 -0800

    changing to chrome as browser and performing login to facebook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
-    let driver = await new Builder().forBrowser('safari').build();
+    let driver = await new Builder().forBrowser('chrome').build();
     try {
-        await driver.get('http://www.google.com/ncr');
-        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-    } finally {
+        await driver.get('https://www.facebook.com/login/');
+
+        //await driver.findElement(By.css('[aria-label="Comment by Guy Shpigler a week ago"] [aria-label="More"]')).click();
+        await driver.findElement(By.css('[aria-label="Email or Phone Number"]')).sendKeys('guy20056@walla.com');
+        await driver.findElement(By.css('[aria-label="Password"]')).sendKeys('dhtjnusntus20',Key.RETURN);
+                await driver.sleep(40000);
+        //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } catch (e) {
+    console.log(e);
+    }finally {
         await driver.quit();
     }
 })();
