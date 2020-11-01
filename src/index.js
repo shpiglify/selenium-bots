e008496239938c38860dc4db5afcdad20f734a04
shpiglify <shpiglify@gmail.com>
2020-11-01 22:48:30 +0200

    adding lost package.json and updating index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
-    let driver = await new Builder().forBrowser('firefox').build();
+    let driver = await new Builder().forBrowser('safari').build();
     try {
         await driver.get('http://www.google.com/ncr');
-        // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-        // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
     } finally {
         await driver.quit();
     }
